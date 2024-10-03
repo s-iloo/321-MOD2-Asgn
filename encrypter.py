63b9a4cf5f8173f86ac981106b33d920907ae2e5
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2024-10-03 14:12:20 -0700

    bit printing

--- encrypter.py
+++ encrypter.py
@@ -15,11 +15,11 @@ def read_text(file_path, mode):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
-            print(f" content is {content}")
+            print(f"content is \n{content}\n")
     except FileNotFoundError:
         print(f"Error: file not found {file_path}")
 
-    # TODO handle header size (54 bytes)
+    # TODO handle header size (54 bytes). We are encrypting .bmp files, not .txt files
     encode_text(content, file_path, mode)
 
 
@@ -27,9 +27,9 @@ def read_text(file_path, mode):
 # runs corresponding encryption algo
 def encode_text(content, file_path, encryption_mode):
     if encryption_mode == EncryptionMode.ECB:
-        encode_text_ecb(content, file_path)
+        return encode_text_ecb(content, file_path)
     elif encryption_mode == EncryptionMode.CBC:
-        encode_text_cbc(content, file_path)
+        return encode_text_cbc(content, file_path)
     else:
         print(f"unrecognized encryption mode: {encryption_mode}")
 
@@ -41,15 +41,13 @@ def encode_text_ecb(content, file_path):
 
     # split the plain text content into 128 bit blocks
     file_size = os.stat(file_path).st_size
-    # TODO what are we using this for?
-    bytes_to_pad = (16 - file_size) / 8
     print(f"file size is {file_size}")
 
     # padding is in whole bytes, the value of each added byte is the
     # number of bytes that are added, i.e. N bytes, each of value N are added
     if file_size % 16 != 0:
         content = pad(content.encode('utf-8'), 16, 'pkcs7')
-        print(f"padded content is {content}")
+        print(f"padded content is {content.hex()}")
 
     # TODO xor content?
 
@@ -61,22 +59,41 @@ def encode_text_ecb(content, file_path):
     # loop through 128 bits (16 bytes) at a time and
     # encrypt each 128 bit block with the generated key
     for i in range(0, len(content), 16):
-        # TODO calculate block
-        block = 0
-        # TODO encrypt call not working bc of block
+        block = content[i:i + 16]
         encrypted_byte_array += ecb_cipher.encrypt(block)
 
-    # TODO want to write the encrypted data to a binary file
-    # should we prompt the user for a file name?
-    # either:
-    # 1) prompt
-    # 2) write to same exact file every time (idk about this)
-    # 3) generate file name (kinda interesting)
+    print(f"encrypted byte array is {encrypted_byte_array.hex()}")
+
+    print("bits are ", end="")
+    print_bits_from_byte_array(encrypted_byte_array)
+
+    print(f"DECRYPTOED: {ecb_cipher.decrypt(encrypted_byte_array)}")
+
+    return encrypted_byte_array
+
+
+# TODO want to write the encrypted data to a binary file (as another function)
+# should we prompt the user for a file name?
+# either:
+# 1) prompt
+# 2) write to same exact file every time (idk about this)
+# 3) generate file name (kinda interesting)
 
 
 def encode_text_cbc(content, file_path):
     return
     # TODO cbc
+
+
+def print_bits_from_byte_array(barray):
+    for byte in barray:
+        for i in range(0, 8):
+            if byte:
+                print(f"{byte & 1}", end="")
+            else:
+                print("0", end="")
+            byte >>= 1
+    print("\n")
 
 
 def handle_mode_input():
